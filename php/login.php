06b4587fdb4361df33e141e67c5e25682164205e
Adebisi Oluwadunmininu <105823967+Oluwadunmininu@users.noreply.github.com>
2022-06-02 22:02:02 +0100

    Updates

--- php/login.php
+++ php/login.php
@@ -1,19 +1,44 @@
 <?php
-if(isset($_POST['submit'])){
-    $username = //finish this line
-    $password = //finish this
+// initiate session
+session_start();
 
-loginUser($email, $password);
 
+if (isset($_POST['submit'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    loginUser($email, $password);
 }
 
-function loginUser($email, $password){
+function loginUser($email, $password)
+{
     /*
         Finish this function to check if username and password 
     from file match that which is passed from the form
     */
+
+    // file path
+    $csv_path = "../storage/users.csv";
+    // get all user details from csv
+    $file_res = fopen($csv_path, 'r');
+    // save data into csv file
+    while ($users = fgetcsv($file_res, 1000)) {
+        # check if line is not empty
+        if (isset($users[0])) {
+            // check if user exist
+            if ($email == $users[1] && $password == $users[2]) {
+                // save user detail to session
+                $_SESSION['username'] = $users[0];
+                // redirect user to dashboard
+                header('location: ../dashboard.php');
+                // stop execution
+                exit;
+            }
+        }
+    };
+    // close file stream
+    fclose($file_res);
+    
+    // invalid login redirect user to login page
+    header('location: ../forms/login.html');
 }
-
-echo "HANDLE THIS PAGE";
-
-mmmm
--- php/logout.php
+++ php/logout.php
@@ -1,10 +1,31 @@
 <?php
-function logout(){
+session_start();
+
+function logout()
+{
     /*
 Check if the existing user has a session
 if it does
 destroy the session and redirect to login page
 */
+
+    // check if user is logged in
+    if(isset($_SESSION['username'])) {
+        // delete user datails
+        unset($_SESSION['username']);
+        // destroy session
+        session_destroy();
+        // redirect user to login page
+        header('location: ../forms/login.html');
+    } 
+
+    else {
+        // destroy session
+        session_destroy();
+        // redirect user to login page
+        header('location: ../forms/login.html');
+    }
 }
 
-echo "HANDLE THIS PAGE";
+// call logout function
+logout();
